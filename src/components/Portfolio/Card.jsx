0e1b392f545a7cd5ca49c1beddf95bbc9a3e213d
Sarah Reed <sarah.reed@crowinsight.com>
2024-08-15 17:46:46 -0600

    added loading message for card images

--- src/components/Portfolio/Card.jsx
+++ src/components/Portfolio/Card.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 function Card({ item, style, onClick, portfolioType }) {
+  const [loading, setLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setLoading(false);
+  };
+
   return (
     <button
       className={`card ${portfolioType}`}
       style={style}
       onClick={portfolioType != "Wood" ? onClick : ""}
     >
-      <img src={item.Thumbnail} alt={item.Title} />
+      {loading && <div className="loading">Loading...</div>}
+      <img src={item.Thumbnail} alt={item.Title} onLoad={handleImageLoad} />
       {item.Title && (
         <div className="card-content">
           <h2 className="card-title">{item.Title}</h2>
