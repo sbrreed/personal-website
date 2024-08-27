86f256ec51b98037bdd55270fa28bf3b9359086a
Sarah Reed <sarah.reed@crowinsight.com>
2024-08-26 20:22:59 -0600

    update resume

--- src/components/Routes/Resume.jsx
+++ src/components/Routes/Resume.jsx
@@ -1,9 +1,9 @@
 export default function Resume() {
   return (
-    <a href="/2024_08_15_SReed_Resume.pdf" target="_blank">
+    <a href="/2024_08_20_SReed_Resume.pdf" target="_blank">
       <img
         className="resume"
-        src="/2024_08_15_SReed_Resume.jpg"
+        src="/2024_08_20_SReed_Resume.jpg"
         alt="Sarah Reed Resume"
       />
     </a>
