615b858535ce8bc70d6f60ff1cf7501301b9791e
Sarah Reed <sarah.reed@crowinsight.com>
2024-10-17 13:51:26 -0600

    home page content and style improvements

--- src/components/Routes/Home.jsx
+++ src/components/Routes/Home.jsx
@@ -4,6 +4,50 @@ export default function Home() {
       <div className="homePageContent">
         <div className="introduction">
           <p>
+            There are many names for what I love to do: Information storyteller,
+            Data Visualization Developer, Data Journalist. My specialty is
+            online interactive tools for presenting information. ( I also create
+            in print and video).
+          </p>
+          <p>
+            I have always been curious, driven by a love for learning and
+            building throughout my life as a woodworking entrepreneur turned
+            MIT-mechanical engineer, high-school math teacher, and student.
+          </p>
+
+          <p>
+            My career has been guided by the question:
+            <nbsp />
+            <span className="homeTextQuestion">
+              How can I achieve my goals with my current tools and resources?
+            </span>
+          </p>
+
+          <p>With data storytelling I always ask two additional questions:</p>
+          <ul>
+            <li>
+              <span className="homeTextQuestion">
+                What is the most important information that this data can tell
+                us?
+              </span>
+            </li>
+            <li>
+              <span className="homeTextQuestion">
+                What is the most effective way to communicate this information?
+              </span>
+            </li>
+          </ul>
+
+          <p>
+            And from these answers, I might write a short article with some
+            simple line charts, or create a more complex interactive experience
+            that walks my audience through intuitive steps to deepen their
+            understanding. There is no one size fits all when it comes to
+            information storytelling. It comes down to the needs of the
+            information and the audience. We are story-tellers with a unique
+            story-telling medium.
+          </p>
+          {/* <p>
             Hello! I am a data visualization consultant. I've been a mechanical
             engineer, woodworker, teacher and student, but through it all I have
             been <i>curious</i>. I'm driven by a love for learning and building.{" "}
@@ -21,8 +65,8 @@ export default function Home() {
             <span className="homeTextQuestion">
               What is the most important information that this data can tell us?
             </span>{" "}
-            We are story-tellers with a unique story-telling medium.
-          </p>
+            
+          </p> */}
         </div>
         {/* <img
           className="profilePicture"
